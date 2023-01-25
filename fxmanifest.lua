7c54020ba33a4fbc56d91ca34f930449f3922b7b
Tennessin <101592901+Tennessin@users.noreply.github.com>
2023-01-25 01:47:45 +0000

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+
+
+fx_version 'adamant'
+games { 'gta5' };
+
+name 'Cone Head';
+description 'Puts a cone on players head.'
+version '1.0.0'
+
+contributor {
+    'Tennessin',
+    'Jordan Micheal'
+};
+
+client_scripts {
+    "cl_conehead",
+
+}
+
