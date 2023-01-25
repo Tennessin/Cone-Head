8fa35bcf91532f8d42a78a45367213c75b6a64c9
Tennessin <101592901+Tennessin@users.noreply.github.com>
2023-01-25 01:57:15 +0000

    Update conehead.lua

--- conehead.lua
+++ conehead.lua
@@ -12,11 +12,11 @@ RegisterCommand("cone", function()
 		else
 			TaskPlayAnim( player, ad, "idle_a", 8.0, 1.0, -1, 49, 0, 0, 0, 0 )
 			Wait (500)
-            AttachEnityToEntity(CreateObject(GetHashKey("prop_roadcone01a"), 0, 0, 0, true, true , true), GetPlayerPed(-1), GetPedBoneIndex(GetPlayerPed(-1), 31086), 0,1 0,1 0, 270, 0, 0, true, true, false, true, 1, true)
-            Citizen.Wait(50500)
-            ClearPedTasks((GetPlayerPed(-1)))
-            DeleteEntity((CreateObject(GetHashKey("prop_roadcone01a"), 0, 0, 0, true, true, true)))
-            ShowNotification('You have been ~r~red~s~ coned')
+            		AttachEnityToEntity(CreateObject(GetHashKey("prop_roadcone01a"), 0, 0, 0, true, true , true), GetPlayerPed(-1), GetPedBoneIndex(GetPlayerPed(-1), 31086), 0,1 0,1 0, 270, 0, 0, true, true, false, true, 1, true)
+            		Citizen.Wait(50500)
+            		ClearPedTasks((GetPlayerPed(-1)))
+           		DeleteEntity((CreateObject(GetHashKey("prop_roadcone01a"), 0, 0, 0, true, true, true)))
+            		ShowNotification('You have been ~r~red~s~ coned')
 		end     
 	end
 end, false)
